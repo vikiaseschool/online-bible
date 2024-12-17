811e64d9c1cef7f0ceae5e679f4c79a2ffd08a9d
vikiaseschool <vyhnalekv@ceskolipska.cz>
2024-12-17 17:58:43 +0100

    Update app.py
    added login/register viewing logic

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from models import get_verse_for_today, get_bible_section, get_book_names_from_file
 
 app = Flask(__name__)
@@ -21,6 +21,36 @@ def bible_text_guest():
 
     text = get_bible_section(book, chapter, verse)
     return jsonify({"text": text})
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+    #ADD LOGIN FUNCTIONALITY
+
+        if username == 'admin' and password == 'password':
+            return redirect(url_for('dashboard'))
+        else:
+            return render_template('login.html', error="Invalid username or password.")
+
+    return render_template('login.html', error=None)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        new_username = request.form['new-username']
+        email = request.form['email']
+        new_password = request.form['new-password']
+
+        # ADD REGISTER FUNCTIONALITY
+
+        return redirect(url_for('login'))
+
+    return render_template('login.html', error=None)
 
 if __name__ == "__main__":
     app.run(debug=True)
